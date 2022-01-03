0e9ce3e8e1155e339f75a3b75cbfc39bf24e4492
matmoimjs <matmoimjs@gmail.com>
2022-01-03 02:36:40 +0200

    api-enums.gloval.ts

--- src/global/api-enums.global.ts
+++ src/global/api-enums.global.ts
@@ -1,9 +1,20 @@
+/* eslint-disable no-shadow */
 // TODO fix eslint for enums
-// eslint-disable-next-line no-shadow
-export enum ApiEnum {
+export enum OrderStateEnum {
   NEW = 'New order, waiting to be filled',
   FILLED = 'Order fully filled',
   PARTIALLY_FILLED = 'Order partially filled',
   CANCELED = 'Order canceled',
   PARTIALLY_CANCELED = 'Order filled partially, and then the rest of the order is canceled'
+}
+
+export enum OrderTypeEnum {
+  LIMIT_ORDER = 'Limit price order',
+  POST_ONLY = 'Post only maker order',
+  IMMEDIATE_OR_CANCEL = 'Immediate or cancel',
+}
+
+export enum TradeTypeEnum {
+  BID = 'Buy',
+  ASK = 'Sell'
 }
