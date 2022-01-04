fd153bfa31db747368695833d627389431d97877
matmoimjs <matmoimjs@gmail.com>
2022-01-04 04:49:18 +0200

    add redis to project

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,14 @@
+import { REDIS } from './common/redis';
 import { pause } from './global/utils/pause.util';
 import { MexSocket } from './ws/mex-socket';
 
 export async function main() {
+    console.log(REDIS.RedisPublisher.status);
     const socket = await MexSocket.open();
-    socket.subscribe['sub.tickers']().onAnswer((answer) => {
-        console.log(answer.data);
-    });
-    await pause(6000);
+    // socket.subscribe['sub.tickers']().onAnswer((answer) => {
+    //     console.log(answer.data);
+    // });
+    // await pause(6000);
     await socket.close();
     await pause(5000);
 }
