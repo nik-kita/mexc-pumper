711ae565cc7f259d1fa8876fba63ad018c7331e0
matmoimjs <matmoimjs@gmail.com>
2022-01-05 21:24:39 +0200

    exit

--- src/cli/driver.cli.ts
+++ src/cli/driver.cli.ts
@@ -2,16 +2,20 @@
 import prompts from 'prompts';
 import { RedisDriverChannel } from '../common/redis/driver-channel.redis';
 import { RedisPublisher } from '../common/redis/redis-publisher';
+import { RedisSubscriber } from '../common/redis/redis-subscriber';
 import { CONTINUE_EXIT_SELECT, PAUSE_EXIT_SELECT, START_EXIT_SELECT } from './select-constants.cli';
 
 const { channel, command } = RedisDriverChannel;
 
 (async () => {
     const { startOrExit } = await prompts(START_EXIT_SELECT);
+    let exitNow = false;
 
     if (startOrExit === RedisDriverChannel.command.exit) {
+        exitNow = true;
         await RedisPublisher.publish(channel, command.exit).then(() => {
             RedisPublisher.disconnect();
+            RedisSubscriber.disconnect();
         });
 
         return;
@@ -19,14 +23,13 @@ const { channel, command } = RedisDriverChannel;
 
     await RedisPublisher.publish(channel, command.start);
 
-    const exitNow = false;
-
     while (!exitNow) {
         const { pauseOrExit } = await prompts(PAUSE_EXIT_SELECT);
 
         if (pauseOrExit === RedisDriverChannel.command.exit) {
             await RedisPublisher.publish(channel, command.exit).then(() => {
                 RedisPublisher.disconnect();
+                RedisSubscriber.disconnect();
             });
 
             return;
@@ -39,6 +42,7 @@ const { channel, command } = RedisDriverChannel;
         if (continueOrExit === RedisDriverChannel.command.exit) {
             await RedisPublisher.publish(channel, command.exit).then(() => {
                 RedisPublisher.disconnect();
+                RedisSubscriber.disconnect();
             });
 
             return;
